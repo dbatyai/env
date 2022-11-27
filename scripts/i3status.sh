743caa2ae0cd5d016af9276f00d2b9530e718048
Daniel Batyai <danielbatyai@gmail.com>
2022-11-27 11:13:10 +0100

    i3status: improve coretemp detection

--- scripts/i3status.sh
+++ scripts/i3status.sh
@@ -5,7 +5,7 @@ set -e
 dir=$(readlink -f "$0" | xargs dirname)
 pushd "$dir" > /dev/null
 
-temp=$(find /sys/devices/platform/coretemp.0 -name temp1_input)
+temp=$(find /sys/devices/platform/coretemp.0 -name 'temp*_input' | sort | head -1)
 if [ ! -z "$temp" ]; then
   ln -sf $temp cpu_temp
 fi
