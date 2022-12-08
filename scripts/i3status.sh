2c1e6a279bf32740e0181766f6040abfcca1311f
Daniel Batyai <danielbatyai@gmail.com>
2022-12-08 09:54:53 +0100

    i3status: improve battery and coretemp

--- scripts/i3status.sh
+++ scripts/i3status.sh
@@ -2,10 +2,16 @@
 
 set -e
 
-dir=$(readlink -f "$0" | xargs dirname)
+dir=/tmp/i3status
+mkdir -p $dir
 pushd "$dir" > /dev/null
 
 temp=$(find /sys/devices/platform/coretemp.0 -name 'temp*_input' | sort | head -1)
 if [ ! -z "$temp" ]; then
   ln -sf $temp cpu_temp
+fi
+
+battery=$(find /sys/class/power_supply/ -name 'BAT*' | sort | head -1)
+if [ ! -z "$battery" ]; then
+  ln -sf "$battery/uevent" battery
 fi
